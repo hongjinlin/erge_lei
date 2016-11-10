0971a44b2751bb3d6957d9d2e3c83fbe43633a8a
hongjl <89757630@qq.com>
2016-11-10 10:25:28 +0800

    hook test

--- githubhook.php
+++ githubhook.php
@@ -3,14 +3,14 @@ $str = file_get_contents('php://input');
 
 // GitHub Webhook Secret.
 // Keep it the same with the 'Secret' field on your Webhooks / Manage webhook page of your respostory.
-$secret = "A*VS5N6./U*2";
+$secret = "YC+5M(4JB7*)";
 
 // Headers deliveried from GitHub
 $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 if ($signature) {
   $hash = "sha1=" . hash_hmac('sha1', $str, $secret);
   if (strcmp($signature, $hash) === 0) {
-      echo shell_exec("cd /data/htdocs/kanjia_dev/ && git pull");
+      echo shell_exec("cd /data/htdocs/erge_lei/ && git pull");
       exit();
   }
 }
